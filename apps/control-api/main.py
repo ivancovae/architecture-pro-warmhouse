153b46342924501a29ade18c2343a9b3f6bb0436
ivancovae <ivancovae@gmail.com>
2025-09-28 02:59:09 +0300

    сервисы для телеметрии и для управления

--- apps/control-api/main.py
+++ apps/control-api/main.py
@@ -1,11 +1,15 @@
+import requests
+import json
+import os
+from typing import Optional, Dict, Any
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-import os
 smarthomeApi = os.getenv("SMARTHOME_APP_API_URL")
- 
+
 app = FastAPI()
- 
+    
 @app.get("/api/")
 def get_about():
     html_content = "Control Devices Service"
@@ -16,12 +20,51 @@ def get_about():
 def get_command(id, command):
     print("id="+id)
     print("command="+command)
+    session = requests.Session()
+
+    url = f"{smarthomeApi}/api/v1/sensors/{id}"
+    print("url="+url)
+    error = ""
+    try:
+        print("request before")
+        response = session.get(url, verify=False)
+        print("request after")
+        response.raise_for_status()
+        print(response)
+        data = response.json() if response.content else {}
+        
+    except requests.exceptions.RequestException as e:
+        error = str(e)
+    if (error != ""):
+        print(error)
     
-    #
-    # sensorRes = requests.get(SMARTHOME_APP_API_URL+"/api/v1/sensors/"+id)
-    # print(sensorRes)
+    status = "inactive"
+    json_data =  json.loads("{}")
 
-    # content = sensorRes.content
-    # sensorStatus = requests.put(SMARTHOME_APP_API_URL+"/api/v1/sensors/"+id, data=content)
+    for key, value in data.items():
+            if key == "status":
+                if value == "inactive":
+                    status = "active"
 
+    json_data["status"] = status
+    print(status)
+    json_data["value"] = 0
+    
+    url = f"{smarthomeApi}/api/v1/sensors/{id}/value?status={status}"
+    print(f"url={url}")
+    try:
+        print("before url")
+        response = session.patch(url, data=None, json=json_data, verify=False)
+        print("after url")
+        response.raise_for_status()
+        print(response)
+
+        status_code = response.status_code
+        data_response = response.json() if response.content else {}
+
+    except requests.exceptions.RequestException as e:
+        error = str(e)
+        if (error != ""):
+            print(error)
+    
     return HTMLResponse(content="Command sended")
--- apps/telemetry-api/index.js
+++ apps/telemetry-api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const process = require('node:process');
+const http = require('http');
+
+const libDb = require('./dbManager.js');
 
 const app = express();
 const router = express.Router();
@@ -7,20 +10,83 @@ const router = express.Router();
 const path = __dirname;
 const port = process.argv[2];
 
+
 TEMPERATURE_API_URL = process.argv[3];
 console.log("TEMPERATURE_API_URL="+TEMPERATURE_API_URL);
 
-router.use(function (req,res,next) {
+const db = new libDb.dbManager();
+
+router.use((req,res,next) => {
   console.log('/' + req.method);
   next();
 });
 
-router.get('/', function(req,res){
+const needSended = [];
+
+router.get(`/api/device/:id/start`, (req,res) => {
+  const id = req.params.id;
+  if (needSended.indexOf(id) == -1) {
+    needSended.push(id);
+  }
+  console.log('id=' + id);
+  
+  console.log('start=' + id);
   res.send('');
+});
+
+router.get(`/api/device/:id/stop`, (req,res) =>{
+  const id = req.params.id;
+  console.log('id=' + id);
+  var index = needSended.indexOf(id);
+  if (index !== -1) {
+    needSended.splice(index, 1);
+  }
+  console.log('stop=' + id);
+  res.send('');
+});
+
+router.get(`/api/device/:id/average`, (req,res) =>{
+  const id = req.params.id;
+  console.log('id=' + id);
+
+  let jsonData = JSON.stringify(db.getSensorById(id), null, 4);  
+  console.log(jsonData);
+  
+  console.log('averange=' + id);
+  res.send('');
+});
+
+router.get('/', (req,res) => {
+  res.send('Service monitoring values');
 });
 
 app.use('/', router);
 
-app.listen(port, function () {
+app.listen(port, () => {
+  db.initialize();
+
   console.log('Listening on port 8082')
+
+  setInterval(()=>{
+    for(let i = 0; i < needSended.length; i++) {
+      var el = needSended[i];
+      console.log(el);
+      const url = `${TEMPERATURE_API_URL}/api/v1/sensors/${el}`;
+      console.log(url);
+      http.get(
+          url,
+          null,
+          (error, response, body) => {
+              if (!error && (response.statusCode == 200)) {
+                  console.log(body);
+                  const json = JSON.parse(body);
+
+                  db.createSensorValue({sensorId: el, value:json.value})
+              } else {
+                console.log(`=> error=${error}`);
+              }
+          }
+      );
+    }
+  }, 1000)
 })
